40a193c370741f73af7ce080861f69dba1616a44
Adarsh Kumar <adarshff06@gmail.com>
2025-05-19 01:06:43 +0530

    Learn the Concept of Routing In Laravel

--- firstProject/routes/web.php
+++ firstProject/routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/about" , function (){
+    return view('about');
+});
+
+// Route::view("/about" , 'about');
+
+Route::get("/user/{name}" , function ($name) {
+    // echo $name;
+    return view('user' , ['name' => $name]);
+});
+
+Route::redirect('/' , '/about');
